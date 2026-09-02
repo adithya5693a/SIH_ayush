13a4c8166623bfe81a28e53fea38d3791360546c
AdithyaND12 <adithyand.cs25@bmsce.ac.in>
2026-09-02 21:06:37 +0530

    Add GOOGLE_API_KEY to os.environ in config.py

--- config.py
+++ config.py
@@ -5,6 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# --- Gemini API Key ---
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "")
+os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+
 # --- Neo4j Connection ---
 NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
 NEO4J_USERNAME = os.getenv("NEO4J_USERNAME", "neo4j")
